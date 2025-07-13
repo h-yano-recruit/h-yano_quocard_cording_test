20859c01ba87c8fed93bfb9275bdfeef2553a611
h-yano-recruit <h-yano-recruit@users.noreply.github.com>
2025-07-13 22:48:55 +0900

    Spring initializr️で生成したファイルを追加

---
+++ src/main/kotlin/com/example/quocard_cording_test/QuocardCordingTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quocard_cording_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuocardCordingTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuocardCordingTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/quocard_cording_test/QuocardCordingTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.quocard_cording_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuocardCordingTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
