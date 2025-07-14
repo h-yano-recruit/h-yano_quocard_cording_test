88fe4c58fe243bdf223cfccc1fa2fba70acadaec
h-yano-recruit <h-yano-recruit@users.noreply.github.com>
2025-07-15 00:25:15 +0900

    作成日時と更新日時の追加

--- src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -1,8 +1,18 @@
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 -- 著者テーブル
 CREATE TABLE authors (
     id            BIGSERIAL PRIMARY KEY,
     name          VARCHAR(255) NOT NULL UNIQUE,
-    date_of_birth DATE         NOT NULL
+    date_of_birth DATE         NOT NULL,
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
 
 -- 書籍テーブル
@@ -11,13 +21,16 @@ CREATE TABLE books (
     title         VARCHAR(255) NOT NULL,
     price         NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
     -- 0が未出版、1が出版済み
-    status        INTEGER      NOT NULL CHECK (status IN (0, 1))
+    status        INTEGER      NOT NULL CHECK (status IN (0, 1)),
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
 
 -- 書籍と著者の関連テーブル
 CREATE TABLE book_authors (
-    book_id   BIGINT NOT NULL,
-    author_id BIGINT NOT NULL,
+    book_id       BIGINT NOT NULL,
+    author_id     BIGINT NOT NULL,
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     PRIMARY KEY (book_id, author_id),
     FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,
     FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE
@@ -27,3 +40,13 @@ CREATE INDEX idx_books_title ON books (title);
 CREATE INDEX idx_authors_name ON authors (name);
 CREATE INDEX idx_book_authors_book_id ON book_authors (book_id);
 CREATE INDEX idx_book_authors_author_id ON book_authors (author_id);
+
+CREATE TRIGGER set_timestamp_authors
+BEFORE UPDATE ON authors
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp_books
+BEFORE UPDATE ON books
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
