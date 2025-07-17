2b18934713dc6f1275fb16b7bbd72fc2dece39ba
h-yano-recruit <h-yano-recruit@users.noreply.github.com>
2025-07-17 20:58:12 +0900

    出版状況の値がOpenAPIから読み取れなかったので説明を追加

--- src/main/kotlin/com/example/quocard_cording_test/model/Book.kt
+++ src/main/kotlin/com/example/quocard_cording_test/model/Book.kt
@@ -2,10 +2,13 @@ package com.example.quocard_cording_test.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
+import io.swagger.v3.oas.annotations.media.Schema
 import java.math.BigDecimal
 import java.time.OffsetDateTime
 
-// 出版状況を表現するEnum
+@Schema(
+    description = "出版状況 (0: 未出版, 1: 出版済み)",
+)
 enum class PublicationStatus(@JsonValue val code: Int) {
     UNPUBLISHED(0),
     PUBLISHED(1);
